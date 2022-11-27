95843271a777d3c5e052ef45edf4342623fa26c7
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-27 21:49:59 +0000

    Updating scrolling page function

--- src/script.js
+++ src/script.js
@@ -1,11 +1,19 @@
 //positioning section when clicking dropdown_img
 $("#dropdown_img").click(function () {
-  var width = window.innerWidth;
+  let height;
+
+  if (window.innerWidth > 768) {
+    height = $("#navbar").height();
+  } else {
+    height = 40;
+  }
+
   var id = $(this).attr("href");
   $("html, body").animate(
     {
-      scrollTop: $(id).offset().top - width,
+      scrollTop: $(id).offset().top - height,
     },
     1000
   );
+  console.log(height);
 });
