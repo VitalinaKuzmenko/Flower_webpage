605de2d0fd62278193530040a713e5b7fd0e21fe
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-27 21:20:41 +0000

    Adding js function for positioning section when clicking dropwdown_img

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-
+//positioning section when clicking dropdown_img
+$("#dropdown_img").click(function () {
+  var width = window.innerWidth;
+  var id = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(id).offset().top - width,
+    },
+    1000
+  );
+});
