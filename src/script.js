b652c97afc5abbbdb3a448a5d198060ebb7b747f
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-29 00:00:28 +0000

    Creating an accordion faq at faq.html

--- src/script.js
+++ src/script.js
@@ -16,3 +16,29 @@ $("#dropdown_img, #about_link, #contact_link").click(function () {
   );
   console.log(height);
 });
+
+// faq accordion
+$(".accordion__header").click(function (e) {
+  e.preventDefault();
+  var currentIsActive = $(this).hasClass("is-active");
+  $(this).parent(".accordion").find("> *").removeClass("is-active");
+  if (currentIsActive != 1) {
+    $(this).addClass("is-active");
+    $(this).next(".accordion__body").addClass("is-active");
+  }
+});
+
+///search panel
+function search_animal() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("animals");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
